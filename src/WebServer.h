eb339156965512849c4ddfdae6a9331fab7fee76
Larry Davis <lazdnet@gmail.com>
2023-02-27 17:10:05 -0800

    Add interface to retrieve log file listings, log files, session files

--- src/WebServer.h
+++ src/WebServer.h
@@ -82,6 +82,60 @@ public:
     }
   }
 
+  String getDataFiles() {
+    String jsonData = String();
+    jsonData += '[';
+
+    File root = SD.open("/");
+    uint fileCount = 0;
+    while (true) {
+      File entry = root.openNextFile();
+
+      if (!entry) {
+        // no more files
+        break;
+      }
+
+      if (!entry.isDirectory()) {
+        const char *name = entry.name();
+        if (strlen(name) >= 3 && strncmp("log", name, 3) == 0) {
+          if (fileCount != 0) {
+            jsonData += ',';
+          }
+          jsonData += '{';
+          jsonData += "\"name\":";
+          jsonData += "\"";
+          jsonData += name;
+          jsonData += "\"";
+          jsonData += ',';
+          jsonData += "\"size\":";
+          jsonData += entry.size();
+          jsonData += '}';
+          fileCount++;
+        }
+      }
+
+      entry.close();
+    }
+    jsonData += ']';
+    return jsonData;
+  }
+
+  String getDataFile(const char *fileName) {
+    String fullFileName = String('/');
+    fullFileName += fileName;
+
+    String fileContents = String();
+    File file = SD.open(fullFileName.c_str(), FILE_READ);
+    if (file) {
+      while (file.available()) {
+        fileContents += (char)file.read();
+      }
+      file.close();
+    }
+    return fileContents;
+  }
+
   void start() {
     server = new AsyncWebServer(80);
     ws = new AsyncWebSocket("/ws");
@@ -92,6 +146,46 @@ public:
     // Route for root / web page
     server->on("/", HTTP_GET, [this](AsyncWebServerRequest *request) {
       request->send_P(200, "text/html", index_html, std::bind(&WebServer::templateProcessor, this, std::placeholders::_1));
+    });
+
+    server->on("/data.json", HTTP_GET, [this](AsyncWebServerRequest *request) {
+      // Root listing
+      request->send(200, "application/json", getDataFiles());
+    });
+
+    server->on("/data", HTTP_GET, [this](AsyncWebServerRequest *request) {
+      if (request->hasParam("download")) {
+        // File request
+        const char *fileName = request->getParam("download")->value().c_str();
+
+        // Only allow log files and the sessions file
+        if ((strlen(fileName) == 18 && strncmp("log-", fileName, 4) == 0) ||
+            (strlen(fileName) == 12 && strncmp("sessions.csv", fileName, 12) == 0)) {
+          String fileContents = getDataFile(fileName);
+          if (fileContents.length() == 0) {
+            Serial.printf("404: %s not found\n", fileName);
+            request->send(404);
+          } else {
+            Serial.printf("200: %s requested\n", fileName);
+
+            String contentDisposition = "attachment; filename=\"";
+            contentDisposition += fileName;
+            contentDisposition += "\"";
+
+            AsyncWebServerResponse *response = request->beginResponse(200, "text/csv", fileContents);
+            response->addHeader("Content-Disposition", contentDisposition);
+
+            request->send(response);
+          }
+        } else {
+          Serial.printf("403: Forbidden file requested: %s\n", fileName);
+          request->send(403);
+        }
+      }
+      else {
+        Serial.println("400: /data request missing download parameter");
+        request->send(400);
+      }
     });
 
     // Start server
