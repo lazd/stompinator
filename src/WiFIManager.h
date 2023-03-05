22e4d046a752ab8c2f78e80ddf923e1decd9e6b3
Larry Davis <lazdnet@gmail.com>
2023-03-05 08:59:04 -0800

    Hopefully fix WiFi connectivity by setting autoReconnect and disabling sleep

--- src/WiFIManager.h
+++ src/WiFIManager.h
@@ -9,10 +9,15 @@ class WiFiManager {
 public:
   void start() {
     M5.Lcd.printf("Connecting to %s\n", SSID);
+    WiFi.setAutoReconnect(true);
     WiFi.begin(SSID, PASSPHRASE);
     while (WiFi.status() != WL_CONNECTED) {
       M5.Lcd.print(".");
       delay(500);
+    }
+    if(WiFi.getSleep() == true) {
+      WiFi.setSleep(false);
+      Serial.println("WiFi sleep disabled");
     }
     M5.Lcd.printf("\nIP %s\n", WiFi.localIP().toString().c_str());
     Serial.printf("IP %s\n", WiFi.localIP().toString().c_str());
